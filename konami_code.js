8b2c3e1185db18b33f508f6863162be30702ca0f
Devan <devan.moylan@backyardlion.com>
2017-08-24 16:37:49 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,9 +4,7 @@ let index = 0;
 
 
 function init() {
-  const input = document.querySelector('body');
-
-  input.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
